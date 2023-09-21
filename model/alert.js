d677da6c8394865e28d4941eaf8f03caabdc2025
Rishi <rishikush31@gmail.com>
2023-09-22 00:50:05 +0530

    alert schema update

--- model/alert.js
+++ model/alert.js
@@ -13,6 +13,12 @@ const alertSchema = new mongoose.Schema({
 
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' },
 
+  state: { 
+    type: String, enum: ['completed','ongoing'], required: true ,default: 'ongoing'
+  },
+
+  relatedRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Request'}],  
+
 }, { timestamps: true });
 
 alertSchema.index({ location: '2dsphere' });
